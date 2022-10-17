d77bf3db4ff2a715d1684264688d811f0b55e364
jwood7 <jwood7@stevens.edu>
2022-10-17 16:03:12 -0400

    Deleted extra files

--- nextjs/pages/bye.js
+++
@@ -1,9 +0,0 @@
-import Layout from '../components/MyLayout';
-
-const Bye = () => (
-  <Layout>
-    <h1>Bye</h1>
-  </Layout>
-);
-
-export default Bye;
--- nextjs/pages/hello.js
+++
@@ -1,9 +0,0 @@
-import Layout from '../components/MyLayout';
-
-const Hello = () => (
-  <Layout>
-    <h1>Hello</h1>
-  </Layout>
-);
-
-export default Hello;
